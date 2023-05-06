2faeea017020ef3c56ba4f89718afab3535499bc
송재혁 <wakij6587@naver.com>
2023-05-06 12:25:51 +0900

    init next

--- src/components/Post/Contents/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-import './index.scss';
-
-const Contents = ({ html }: { html: string }) => {
-  return <div className="post-contents" dangerouslySetInnerHTML={{ __html: html }} />;
-};
-
-export default Contents;
